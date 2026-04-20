106fa7d0dfb361ab0a7acecb48d4731671bea768
MartinPirate <sirmartins6@gmail.com>
2026-04-20 12:49:59 +0300

    feat(demo): wire SpodLite<Client> and AuthGate routing between sign-in and posts

---
+++ demo_app/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:spod_lite_client/spod_lite_client.dart';
+import 'package:spod_lite_sdk/spod_lite_sdk.dart';
+
+import 'screens/posts_screen.dart';
+import 'screens/sign_in_screen.dart';
+import 'theme.dart';
+
+late final SpodLite<Client> spod;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  spod = SpodLite<Client>(
+    createClient: () => Client('http://localhost:8088/')
+      ..connectivityMonitor = FlutterConnectivityMonitor(),
+    adminEndpoint: (c) => c.adminAuth,
+  );
+  await spod.auth.restore();
+  runApp(const DemoApp());
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spod Demo',
+      debugShowCheckedModeBanner: false,
+      theme: buildDemoTheme(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  @override
+  void initState() {
+    super.initState();
+    spod.auth.events.listen((_) {
+      if (mounted) setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!spod.auth.isSignedIn) {
+      return SignInScreen(onSignedIn: () => setState(() {}));
+    }
+    return PostsScreen(
+      onSignOut: () async {
+        await spod.auth.signOut();
+        if (mounted) setState(() {});
+      },
+    );
+  }
+}
